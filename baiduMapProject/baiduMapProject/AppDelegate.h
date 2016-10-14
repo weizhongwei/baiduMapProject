7db465d4424317ab7a393052d6463d7e930fb221
PEIYANG <peiyang@peiyangdemac-pro.local>
2016-10-14 16:43:44 +0800

    初始化项目

---
+++ baiduMapProject/baiduMapProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baiduMapProject
+//
+//  Created by PEIYANG on 16/10/14.
+//  Copyright © 2016年 YP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
